953439326a84b489115529e860580122ccb661cb
Dina Villanueva <dinarociovq15@hotmail.com>
2022-09-05 23:45:26 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2>Hello World</h2>
+</template>
+
+<style scoped>
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+}
+</style>
---
+++ src/components/View.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  element: string;
+  ariaLabel?: string;
+  isDisable?: boolean;
+}>();
+</script>
+
+<template>
+  <component :is="element" :aria-label="ariaLabel" :disable="isDisable">
+  </component>
+</template>
